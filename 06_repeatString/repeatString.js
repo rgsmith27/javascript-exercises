80275fb64c80b63ee41ea6abdb60282d62f850e3
rgsmith27 <149701337+rgsmith27@users.noreply.github.com>
2025-07-19 11:48:33 -0400

    pass all tests and complete exercise

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, repeat) {
+    if (repeat < 0){
+        return 'ERROR';
+    }
+    var resultString = '';
+    for(let i = 0; i < repeat; i++){
+        resultString = resultString + string;
+    }
+    return resultString;
 };
 
 // Do not edit below this line
