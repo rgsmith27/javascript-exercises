387d344abcb27f73a84ebd268907e0e108f9537d
rgsmith27 <149701337+rgsmith27@users.noreply.github.com>
2025-07-19 12:28:58 -0400

    pass all test

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,8 +1,9 @@
-const removeFromArray = function(list, ...args) {
-    return(list
-        .filter((item) => 
+const removeFromArray = function (list, ...args) {
+    return (list
+        .filter((item) =>
             !args.includes(item)
-        ));
+        )
+    );
 };
 
 // Do not edit below this line
