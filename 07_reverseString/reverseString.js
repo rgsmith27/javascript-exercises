559e902210bc6f21271c4a9079d29f2af3d6412f
rgsmith27 <149701337+rgsmith27@users.noreply.github.com>
2025-07-19 11:55:23 -0400

    pass all test

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    wordArray = word.split('');
+    reverseWord = "";
+    for(let i = wordArray.length; i--; i > -1){
+        reverseWord = reverseWord + wordArray[i];
+    }
+    return reverseWord;
 };
 
 // Do not edit below this line
