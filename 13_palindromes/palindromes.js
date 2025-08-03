17fc29a7a5c53d237bd50aec631385f8058fa706
rgsmith27 <149701337+rgsmith27@users.noreply.github.com>
2025-08-03 18:13:42 -0400

    complete exercise

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,20 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    word = word.replace(/[!?.,;: ]/g, '').toLowerCase();
+    let forwardPointer = 0;
+    let backwardPointer = word.length - 1;
+    while(backwardPointer > forwardPointer){
+        let backwardChar = word.charAt(backwardPointer);
+        let forwardChar = word.charAt(forwardPointer);
+        if(backwardChar != forwardChar){
+            return false;
+        }
+        backwardPointer -= 1;
+        forwardPointer += 1;
+    }
+    return true;
 };
+
+palindromes('racecar');
 
 // Do not edit below this line
 module.exports = palindromes;
