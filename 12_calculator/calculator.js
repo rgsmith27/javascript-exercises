1d58c8f3edc358123f4aae87fc5a76d61ca1f489
rgsmith27 <149701337+rgsmith27@users.noreply.github.com>
2025-08-03 17:50:49 -0400

    complete exercise

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	let sum = 0;
+  numbers.forEach(num => {
+    sum += num;
+  });
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  let product = 1;
+  numbers.forEach(num => {
+    product *= num;
+  });
+  return product
 };
 
-const power = function() {
-	
+const power = function(base, exp) {
+  let value = 1;
+	for(let i = 0; i < exp; i++){
+    value *= base;
+  }
+  return value;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let value = 1;
+  for(let i = 1; i <= num; i++){
+    value *= i;
+  }
+  return value;
 };
 
 // Do not edit below this line
