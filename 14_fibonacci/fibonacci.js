21862f005a155dd949073656cf17f5838efe9e66
rgsmith27 <149701337+rgsmith27@users.noreply.github.com>
2025-08-03 18:25:08 -0400

    complete exercise

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+   let sequence = [0, 1, 1];
+   if(num < 0){
+    return("OOPS");
+   }
+   while(sequence.length - 1 < num){
+    next = sequence[sequence.length - 1] +  sequence[sequence.length - 2];
+    sequence.push(next);
+   }
+   return sequence[num];
 };
 
 // Do not edit below this line
