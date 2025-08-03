cd0c7cf7bba5df32e335259f9a62d94f1d2a10e0
rgsmith27 <149701337+rgsmith27@users.noreply.github.com>
2025-08-03 18:44:54 -0400

    complete exercise

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+function getAge(person){
+    let currentYear = new Date().getFullYear();
+    let age = 0;
+    if (!person.yearOfDeath){
+        age = currentYear - person.yearOfBirth;
+    }
+    else{
+        age = person.yearOfDeath - person.yearOfBirth;
+    }
+    console.log(age);
+    return age;
+}
 
+const findTheOldest = function(people) {
+    return people.reduce((oldest, person) => {
+        return getAge(person) > getAge(oldest) ? person : oldest;
+    });
 };
 
 // Do not edit below this line
